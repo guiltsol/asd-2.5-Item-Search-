43528cf96058d7e785a8c03db832a1f5876eb49c
guiltsol <maksimgusev123123@mail.ru>
2023-02-23 15:08:51 +0300

    Add project files.

---
+++ asd 2.5/Source.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <time.h>
+using namespace std;
+void search(int* arr, int i,int el) {
+	int h = 0;
+	for (int j = 0;j < i;j++) {
+		if (el == arr[j]) {
+			cout << "Item "<<arr[j]<<" found and its number : " << j+1 << endl;
+			h++;
+			
+		}
+	}
+	if (h == 0) cout << "Element "<<el<<" not found!" << endl;
+}
+int main() {
+	int* a;
+	int x,n,s;
+	srand(time(NULL));
+	cout << "Enter the size of the array: " << endl;
+	cin >> n;
+	a = new int[n];
+	for (int j = 0;j < n;j++) {
+		x = rand() % 50;
+		a[j] = x;
+	}
+	cout << "Array: ";
+	for (int j = 0; j < n;j++)
+	{
+		cout << a[j] << " ";
+	}
+	cout << endl;
+	cout << endl;
+	cout << "Enter the number of the item you want to find: " << endl;
+	cin >> s;
+	search(a, n, s);
+	cout << endl;
+	system("pause");
+}
